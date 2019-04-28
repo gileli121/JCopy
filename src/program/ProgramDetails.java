699e50a209fe236390640d9f5fddc817788855c9
Gil Eliyahu <gil1eli@hotmail.com>
2019-04-28 15:34:32 +0300

    Update ProgramDetails.java

--- src/program/ProgramDetails.java
+++ src/program/ProgramDetails.java
@@ -1,8 +1,8 @@
 package program;
 
 public class ProgramDetails {
-    public static final String NAME = "CopyHelper";
+    public static final String NAME = "JCopy";
     public static final String VERSION = "v0.1";
-    public static final String WEBSITE = "http://copyhelper.info";
-    public static final String WEBSITE_DISPLAY = "www.CopyHelper.info";
+    public static final String WEBSITE = "https://github.com/gileli121/JCopy";
+    public static final String WEBSITE_DISPLAY = "github.com/gileli121/JCopy";
 }
